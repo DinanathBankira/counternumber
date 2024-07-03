9706b15813d90f6ec9d0ed514b25dbd50df8abc7
Dina <dinanath88090@outlook.com>
2024-07-03 06:25:26 +0530

    decriment

--- src/components/CounterComponents.jsx
+++ src/components/CounterComponents.jsx
@@ -4,11 +4,13 @@ import React, { useState } from 'react';
 
 const CounterComponents=()=>{
     const [count,setCount]=useState(12);
+    console.log(count);
 
     return(
         <div>
             <h1>Count Component - {count}</h1>
             <button onClick={()=>setCount(count+1)}>Incriment</button>
+            <button onClick={()=>setCount(count-1)}>Decriment</button>
         </div>
     );
 }
